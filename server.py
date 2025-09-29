b9c9c2defcc54729037a4246c3c493658f4e0425
khushbu <talaviyakhushbu19@gmail.com>
2025-09-28 22:45:10 -0500

    define flask new transaction and minning block

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request    
 import Blockchain
 from uuid import uuid4  
 import hashlib
@@ -17,12 +17,53 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods = ['GET'])
 def mine():
-    return "We will mine a new Block"
+    #calculate the proof of work
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+
+    proof = blockchain.proof_of_work(last_proof)
+
+    #rewarding the miner 'considiring 0 as a system'
+
+    blockchain.new_transaction(sender = "0", recipient = node_identifier, amount = 1)
+
+    #forge the new block by adding into the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+    
+    response = {
+        'message': 'New Block Forged', 
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash']
+
+    }
+    return jsonify(response), 200
+
+
 
 
 @app.route('/transaction/new', methods = ['POST'])
 def new_transaction():
-    return "We will add a new transaction"
+    
+    values = request.get_json()
+
+    #check all the requirements are met 
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing Values', 400
+    
+    #create a new transcation 
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'meesage': f'Transcation will be added at {index}'}
+
+    return jsonify(response), 201
+
+
+
+
 
 @app.route('/chain', methods=['GET'])
 def chain():
